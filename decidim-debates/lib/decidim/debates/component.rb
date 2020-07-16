5e997241d2ad444be95de7c7ab6f14a249e3c784
Oriol Gual <oriol@codegram.com>
2020-07-16 17:06:43 +0200

    Endorse debates

--- decidim-debates/lib/decidim/debates/component.rb
+++ decidim-debates/lib/decidim/debates/component.rb
@@ -18,6 +18,8 @@ Decidim.register_component(:debates) do |component|
   end
 
   component.settings(:global) do |settings|
+    settings.attribute :endorsements_enabled, type: :boolean, default: true
+    settings.attribute :endorsements_blocked, type: :boolean
     settings.attribute :comments_enabled, type: :boolean, default: true
     settings.attribute :comments_max_length, type: :integer, required: false
     settings.attribute :announcement, type: :text, translated: true, editor: true
@@ -38,13 +40,19 @@ Decidim.register_component(:debates) do |component|
     Decidim::Follow.where(decidim_followable_type: "Decidim::Debates::Debate", decidim_followable_id: debates_ids).count
   end
 
+  component.register_stat :endorsements_count, priority: Decidim::StatsRegistry::MEDIUM_PRIORITY do |components, start_at, end_at|
+    debates_ids = Decidim::Debates::Debate.where(component: components).not_hidden.pluck(:id)
+    Decidim::Endorsement.where(resource_id: debates_ids.pluck(:id), resource_type: Decidim::Debates::Debate.name).count
+  end
+
   component.register_resource(:debate) do |resource|
     resource.model_class_name = "Decidim::Debates::Debate"
     resource.card = "decidim/debates/debate"
     resource.searchable = true
+    resource.actions = %w(create endorse)
   end
 
-  component.actions = %w(create)
+  component.actions = %w(create endorse)
 
   component.seeds do |participatory_space|
     admin_user = Decidim::User.find_by(
